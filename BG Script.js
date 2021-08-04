dd19b2f489150e5f31efcd5daf6bff59a5818794
i-m-mk <mayank4334@gmail.com>
2021-08-04 15:47:37 +0530

    updating

--- BG Script.js
+++ BG Script.js
@@ -1,11 +1,12 @@
-var color1 = document.querySelector(".color1")
-var color2 = document.querySelector(".color2")
-var bg = document.querySelector(".gradient")
+var css = document.querySelector("h5");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var bg = document.getElementById("gradient");
 
-color1.addEventListener("input", function(){
+function changeBackground() {
 	bg.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
-})
+	css.textContent = bg.style.background;
+}
 
-color2.addEventListener("input", function(){
-	bg.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
-})
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
